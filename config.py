30a954c91a9bb4dff7d1a4089a072807bcd5da18
Norm Ross <bitbucket@normross.ca>
2020-01-16 09:00:40 -0500

    Added database

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'these-are-not-the-droids-you-are-looking-for'
-    
+    SECRET_KEY = (
+        os.environ.get("SECRET_KEY") or "these-are-not-the-droids-you-are-looking-for"
+    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
